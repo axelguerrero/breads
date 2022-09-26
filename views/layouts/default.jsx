a585c80a6cd34cf3c318497b9cf30178e1dc819d
axelguerrero <guerreroaxel230@yahoo.com>
2022-09-26 18:59:56 -0400

    did jsx parts

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -4,11 +4,12 @@ function Default(html) {
     return (
         <html>
             <head>
-                <title> default</title>
+                <title>default</title>
             </head>
             <body>
                 <h1>html rendered!</h1>
-                <div className="container">{html.children}
+                <div className="container">
+                    {html.children}
                 </div>
             </body>
         </html>
